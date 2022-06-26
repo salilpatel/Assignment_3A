411c9349b2351d2b0cca04ce45c16e9dd19a2e7d
Salil Patel <101462890+salilpatel@users.noreply.github.com>
2022-06-26 05:39:18 +0000

    3rd almost done

---
+++ Problem3/problem3.js
@@ -0,0 +1,16 @@
+function generateCard(){
+  let name = document.getElementById("name").value; 
+  let collegeName = document.getElementById("collegeName").value;
+  let location = document.getElementById("location").value;
+  let img = document.getElementById("img");
+  
+  document.getElementById("textGeneratedIdCard").innerHTML = "Generated Id Card";
+  document.getElementById("showName").innerHTML = `Name: ${name}`;
+  document.getElementById("showCollegeName").innerHTML = `Colege Name: ${collegeName}`;
+  document.getElementById("showLocation").innerHTML = `Location: ${location}`;
+
+  document.getElementById("imgDiv").style.backgroundColor = "blue";
+  document.getElementById("infoDiv").style.backgroundColor = "#d9d9d9";
+
+  document.getElementById('myImg').style.display = "block";
+}
